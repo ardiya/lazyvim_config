455a0cb49a2d9bcdfd6b266ee1b23e11e6875b1c
Aditya Ardiya <1798412+ardiya@users.noreply.github.com>
2025-12-07 14:34:42 +0900

    feat: autocmds ctrl-l for clear term
    Signed-off-by: Aditya Ardiya <1798412+ardiya@users.noreply.github.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Ctrl-L to clear terminal screen
+-- https://github.com/LazyVim/LazyVim/issues/4509#issuecomment-2431509475
+vim.api.nvim_create_autocmd("TermEnter", {
+  callback = function(ev)
+    vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = ev.buf, nowait = true })
+  end,
+})
